a8a954101a5dec680025747ff5d8a51584f2c4cd
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-07-13 18:50:21 +0530

    Backend Complete Setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
+import 'dotenv/config';
 import express from 'express';
-import 'dotenv/config'
+import connectDB from './db/Database.js';
+import cookieParser from "cookie-parser";
+import fileUpload from 'express-fileupload';
+
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-// Define a simple route
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({useTempFiles: true}));
+
+connectDB();
+
 app.get('/', (req, res) => {
     res.send("API Working");
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
+ 
